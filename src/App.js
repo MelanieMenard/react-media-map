17e742cfe026dc802b00f722f11baf50d8be9fd0
Melanie Menard <melanie@ocastastudios.com>
2018-08-13 17:21:44 +0100

    install dependencies

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Melanie Test</h1>
+          <h1 className="App-title">React Media Map Prototype</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-content">
+        </div>
       </div>
     );
   }
